596116f802aedd954a06fa8538c1a717b62d7c1c
Brian of London <bol@snnsite.com>
2021-01-04 11:31:37 +0200

    working on podcast index query

--- podcastindex.py
+++ podcastindex.py
@@ -7,6 +7,7 @@ import hashlib
 import json
 import requests
 import time
+import os
 
 # create and parse our args
 parser = argparse.ArgumentParser()
@@ -15,44 +16,88 @@ args = parser.parse_args()
 
 # setup some basic vars for the search api. 
 # for more information, see https://api.podcastindex.org/developer_docs
-api_key = 'YOUR API KEY HERE'
-api_secret = 'YOUR API SECRET HERE'
-query = args.search_query
-url = "https://api.podcastindex.org/api/1.0/search/byterm?q=" + query
 
-# uncomment these to make debugging easier.
-# print ('api key: ' + api_key);
-# print ('api secret: ' + api_secret);
-# print ('query: ' + query);
-# print ('url: ' + url);
+import config
 
-# the api follows the Amazon style authentication
-# see https://docs.aws.amazon.com/AmazonS3/latest/dev/S3_Authentication2.html
-
-# we'll need the unix time
-epoch_time = int(time.time())
-
-# our hash here is the api key + secret + time 
-data_to_hash = api_key + api_secret + str(epoch_time)
-# which is then sha-1'd
-sha_1 = hashlib.sha1(data_to_hash.encode()).hexdigest()
-
-# now we build our request headers
-headers = {
-    'X-Auth-Date': str(epoch_time),
-    'X-Auth-Key': api_key,
-    'Authorization': sha_1,
-    'User-Agent': 'postcasting-index-python-cli'
+baseURL = 'https://api.podcastindex.org/'
+apiCalls = {
+    'byterm' : '/api/1.0/search/byterm?q='
 }
 
-# perform the actual post request
-r = requests.post(url, headers=headers)
+def getHeaders():
+    """ Return the query headers """
+    # uncomment these to make debugging easier.
+    # print ('api key: ' + api_key);
+    # print ('api secret: ' + api_secret);
+    # print ('query: ' + query);
+    # print ('url: ' + url);
 
-# if it's successful, dump the contents (in a prettified json-format)
-# else, dump the error code we received
-if r.status_code == 200:
-    print ('<< Received >>')
-    pretty_json = json.loads(r.text)
-    print (json.dumps(pretty_json, indent=2))
-else:
-    print ('<< Received ' + str(r.status_code) + '>>')
+    # the api follows the Amazon style authentication
+    # see https://docs.aws.amazon.com/AmazonS3/latest/dev/S3_Authentication2.html
+    # we'll need the unix time
+    config.init()
+    api_key = config.pod2api['key']
+    api_secret = config.pod2api['secret']
+    epoch_time = int(time.time())
+
+    # our hash here is the api key + secret + time 
+    data_to_hash = api_key + api_secret + str(epoch_time)
+    # which is then sha-1'd
+    sha_1 = hashlib.sha1(data_to_hash.encode()).hexdigest()
+
+    # now we build our request headers
+    headers = {
+        'X-Auth-Date': str(epoch_time),
+        'X-Auth-Key': api_key,
+        'Authorization': sha_1,
+        'User-Agent': 'postcasting-index-python-cli'
+    }
+    return headers
+
+
+def doCall(call, query):
+    if call in apiCalls:
+        url = baseURL + apiCalls[call] + query
+        r = requests.post(url, headers=getHeaders())
+        return r
+    else:
+        return False
+    
+    
+def doSearch(query):
+    """ run a vanilla search returns a request """
+    url = "https://api.podcastindex.org/api/1.0/search/byterm?q=" + query
+    # perform the actual post request
+    r = requests.post(url, headers=getHeaders())
+    return r
+
+
+if __name__ == "__main__":
+    
+    query = args.search_query
+  
+    # perform the actual post request
+    r = doCall('byterm',query)
+    
+    # if it's successful, dump the contents (in a prettified json-format)
+    # else, dump the error code we received
+    if r.status_code == 200:
+        print ('<< Received >>')
+        pretty_json = json.loads(r.text)
+        countfeeds = pretty_json['count']
+        print(f'Number of feeds: {countfeeds}')
+        print (json.dumps(pretty_json, indent=2))
+    else:
+        print ('<< Received ' + str(r.status_code) + '>>')
+
+    for fdd in pretty_json['feeds']:
+        ftit = fdd['title']
+        fid = fdd['id']
+        furl = fdd['url']
+        print(f'{fid} - {ftit} - {furl}')
+        print('fetching URL for size')
+        r = requests.get(furl)
+        cRSS = r.text
+        lsize = len(cRSS) / 1024
+        print(f'Str Len: {lsize:.0f} kb')
+    
